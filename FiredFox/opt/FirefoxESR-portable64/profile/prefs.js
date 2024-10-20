8c09e31239d8aba670aa5b5df486c89466f604ce
matthewphilipgismondi <matthewphilipgismondi@protonmail.com>
2024-10-20 11:24:22 +0000

    Add files via upload

---
+++ FiredFox/opt/FirefoxESR-portable64/profile/prefs.js
@@ -0,0 +1,44 @@
+// disable default browser check
+user_pref("browser.shell.checkDefaultBrowser", false);
+// use less vertical space
+user_pref("browser.uidensity", 1);
+// render website text while fetching images
+user_pref("nglayout.initialpaint.delay", 0);
+// use less disk space and write less often
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+user_pref("browser.sessionstore.resume_from_crash", false);
+// disable annoyances
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("startup.homepage_welcome_url", "");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.startup.homepage", "/usr/share/doc/home.htm");
+user_pref("trailhead.firstrun.didSeeAboutWelcome", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.screenshots.disabled", true);
+user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
+user_pref("browser.uitour.enabled", false);
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("identity.fxaccounts.toolbar.enabled", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+// privacy
+user_pref("app.update.auto", false);
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("privacy.globalprivacycontrol.was_ever_enabled", true);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("app.normandy.enabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("app.shield.optoutstudies.enabled", true);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("dom.private-attribution.submission.enabled", false);
+user_pref("browser.discovery.enabled", false);
